8d5e6f635800c5e951e6cb31041d0a31e9437f17
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-08 21:39:01 -0700

    fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Reaurn: always 1 (sucess)
+ */
+int main(void)
+{
+		write(2,
+				"and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
