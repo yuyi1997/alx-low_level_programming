c444361bfe642424b3bb93aba35b916fd0164a08
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-09 10:43:25 -0700

    c functions

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = read() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
