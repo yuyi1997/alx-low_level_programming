5323f8be6435d939b9268d9414f65bc4e2d5fb2a
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-09 21:53:03 -0700

    adding

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>0
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Sucesss)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+{
