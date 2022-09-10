3a0cf20998d81f07920c4f59c4c678527aead530
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-10 12:42:12 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: always 0
+ * Return: always 0 (sucess)
  */
 int main(void)
 {
@@ -12,10 +12,10 @@ int main(void)
 		char d;
 		float f;
 
-		printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
-		printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
-		printf("Size of a long int: %d byte(s)\n", (unsigned long) sizeof(b));
-		printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
-		printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(b));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 		return (0);
 }
