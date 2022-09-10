c8fbb322fb2051768eadcfe566abf023ad338d59
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-10 12:31:20 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entry point
  *
- * Reaurn: always 1 (sucess)
+ * Return: always 1 (sucess)
  */
 int main(void)
 {
 		write(2,
-				"and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
