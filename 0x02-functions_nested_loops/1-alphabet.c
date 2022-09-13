d271b29b657f0d23442031c2fc592608aae33188
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-13 09:18:33 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * print_alphabet - prints out the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
