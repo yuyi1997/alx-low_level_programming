3f1f8f1dab5d973517fcde47e5b099aad2ee3721
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-13 09:31:29 -0700

    h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - checks if char is lowercase or upper case
+ * @c: The input char
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	int checks;
+
+	if (isloer(c0 == 0)
+		check = 0;
+	else
+		check = 1;
+	return (check);
+}
