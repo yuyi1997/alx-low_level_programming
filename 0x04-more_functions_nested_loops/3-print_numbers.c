eafc86e35b8461cfe25852b3ff4b3ce970e2db89
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-15 15:14:19 -0700

    l

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * Return: nothing (void)
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
