fd28827436578deb9df1cf67634a9f980762ac8a
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-15 09:43:02 -0700

    k

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - test function that prints
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	int i:
+	int i;
 
 	i = 0;
 	positive_or_negative(i);
