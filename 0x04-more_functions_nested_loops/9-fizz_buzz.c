c8f874e0f964ed267e9810d4821c9bf35f7e6989
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-15 20:54:58 -0700

    l

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 99; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
