ba28c05f11b7c21e4fc7d074a34eb4af033faf21
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-21 11:13:56 -0700

    l

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses
+ * the content of an array of integers
+ * @a: the array of integers
+ * @n: the number of elements of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int hold;
+	int j = n - 1;
+
+	while (i < j)
+	{
+		hold = *(a + i);
+		*(a + i) = *(a + j);
+		i++;
+		j--;
+	}
+}
