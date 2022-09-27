907b5351fb0078415a8ff47d5a5fdefbf5c8745e
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-26 19:43:29 -0700

    l

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -26,5 +26,5 @@ void print_diagsums(int *a, int size)
 		diagonal_sum_2 += a[i];
 	}
 
-	printf("%d, %d,\n", diagonal_sum_1, diagonal_sum_2);
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
 }
