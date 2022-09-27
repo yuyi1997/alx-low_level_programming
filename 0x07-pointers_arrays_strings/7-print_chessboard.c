179ef4d29edc708e2a7edf34cfa80da7b768f386
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-26 18:54:35 -0700

    l

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 {
 	int row, column;
 
-	for  (row = 0; row < 8; colunm++)
+	for  (row = 0; row < 8; row++)
 	{
 		for (column = 0; column < 8; column++)
 			_putchar(a[row][column]);
