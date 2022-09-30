88ce1f08a201e574e839bcd995f47bbcb40ca080
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-30 16:00:02 -0700

    l

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,3 +19,37 @@ bool is_num(char *argvv)
 	}
 	return (1);
 }
+
+/**
+ * main - paint sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i - 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	ptrintf("%d\n", sum);
+
+	return (0);
+}
