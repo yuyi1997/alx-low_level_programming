a7bd8d8f1f526033e5ae68be3308311dcf317e2a
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-30 14:51:19 -0700

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((nused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
