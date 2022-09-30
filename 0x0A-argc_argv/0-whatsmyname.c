75b70a393138a83f13293c77f6fda192a8ec583b
katongwe yuyi longwe <katongwe97@gmail.com>
2022-09-30 14:02:01 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print name of the program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", srgv[0]);
+
+	return (0);
+}
