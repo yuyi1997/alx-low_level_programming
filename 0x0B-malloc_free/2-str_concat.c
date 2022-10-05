9a17da2994bf3496e71a40db217ab0c831619949
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-05 14:53:08 -0700

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 		lens2++;
 		s2++;
 	}
-	s2 = start2;
+	s2 = starts2;
 	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	starts1 = new_str;
 	if (new_str == NULL)
