a0c37c66a023d2500b188a90a13c9c30d1816c38
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-06 15:37:08 -0700

    l

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory
+ * using malloc
+ * @b: an unsigned input integer
+ * Return: a pointer to the allocated memory
+ * or NULL if it fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	if (s == Null)
+		exit(98);
+
+	return (s);
+}
