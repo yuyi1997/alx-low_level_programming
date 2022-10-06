c52b757c43d617d1fef448713d2380cc276eca5a
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-06 15:47:28 -0700

    l

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,17 @@
 
 /**
  * malloc_checked - function that allocates memory
- * using malloc
- * @b: an unsigned input integer
- * Return: a pointer to the allocated memory
+ * using malloc.
+ * @b: an insigned input integer
+ * Retrun: a pointer to the allocated memory
  * or NULL if it fails
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *s;
+
+	s = malloc(b);
 
 	if (s == NULL)
 		exit(98);
