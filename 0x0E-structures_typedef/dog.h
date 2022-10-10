e00bf8e625f3a3962c62e91d0912cf60338f3a62
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-10 14:28:32 -0700

    l

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
