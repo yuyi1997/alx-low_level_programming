f030538e607ca22b814d9893cf30aed90d31f62d
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-20 15:03:31 -0700

    l

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a nnode to the head of the linked list
+ * @head: pointer to the head of list
+ * @str: point to string that adds as node
+ * Return: pointer to new head of list, NULL ot failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	
+	if (str == NULL)
+	       return (NULL);
+	if (strdup(str) == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+
+	if (head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * _strlen -get the length of the string
+ * @str: the string
+ * Return: the length
+ */
+
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
