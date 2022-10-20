8032f587eff95c2fe7167e07ca4ce3bd188e48d7
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-20 14:27:09 -0700

    l

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - a function to print a linked list
+ * @h: pointer to head of list
+ * Return: number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
