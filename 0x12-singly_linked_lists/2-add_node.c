71c2c7713eef8514ea367572dc82e324f6bbfc4d
katongwe yuyi longwe <katongwe97@gmail.com>
2022-10-20 15:13:24 -0700

    l

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,9 +10,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	
+
 	if (str == NULL)
-	       return (NULL);
+		return (NULL);
 	if (strdup(str) == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
