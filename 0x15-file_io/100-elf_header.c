ab9608816246d276116006cec1f69ebfba2021a9
katongwe yuyi longwe <katongwe97@gmail.com>
2022-11-02 14:34:43 -0700

    l

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point for program to get header of EFL file
+ * @argc: the number of arguments
+ * @argv: Then pointer to array of arguments
+ * Return: 1 on success, error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	returb (1);
+}
