8fb8fd585092894207b8aae422423bf290db8e25
katongwe yuyi longwe <katongwe97@gmail.com>
2022-11-04 12:26:44 -0700

    l

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  * Return: 1 on success, -1 if the file can not be created, nor written,
  * nor write fails.
  */
-int appened_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fdo, fdw, len = 0;
 
